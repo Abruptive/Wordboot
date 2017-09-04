6383aa47744ac6474b7b6f5141cb88036333c68b
Alexandru Doda <doda@alexandru.co>
2017-09-04 10:40:47 +0300

    Integrated with Metabun

--- plugin/includes/admin/class-plugin-meta.php
+++ plugin/includes/admin/class-plugin-meta.php
@@ -25,7 +25,7 @@ if( ! class_exists( 'Plugin_Meta' ) ) {
 			$this->load_dependencies();
 
 			// Create an example meta box for the 'item' post type.
-			new ALXWP_Meta( 'item', array(
+			new Metabun( 'item', array(
 				array(
 					'id' => 'section',
 					'title' => 'Meta Boxes',
@@ -34,81 +34,8 @@ if( ! class_exists( 'Plugin_Meta' ) ) {
 							'id'          => 'text',
 							'title'       => 'Text',
 							'type'        => 'text',
-							'description' => 'This is an example text setting.',
-							'default'     => 'Default Value'
+							'description' => 'This is an example text field.'
 						),
-						array(
-							'id'          => 'textarea',
-							'title'       => 'Textarea',
-							'type'        => 'textarea',
-							'description' => 'This is an example textarea setting.',
-							'default'     => 'Default Value'
-						),
-						array(
-							'id'          => 'toggle',
-							'title'       => 'Toggle',
-							'type'        => 'toggle',
-							'description' => 'This is an example toggle setting.',
-						),
-						array(
-							'id'          => 'select',
-							'title'       => 'Select',
-							'type'        => 'select',
-							'description' => 'This is an example select setting.',
-							'options'     => array(
-								array(
-									'id'    => 'option_1',
-									'title' => 'Option 1'
-								),
-								array(
-									'id'    => 'option_2',
-									'title' => 'Option 2'
-								),
-								array(
-									'id'    => 'option_3',
-									'title' => 'Option 3'
-								)
-							),
-							'default' => 'option_2'
-						),
-						array(
-							'id'          => 'checkbox',
-							'title'       => 'Checkboxes',
-							'type'        => 'checkbox',
-							'description' => 'This is an example checkboxes setting.',
-							'options'     => array(
-								array(
-									'id'    => 'option_1',
-									'title' => 'Option 1'
-								),
-								array(
-									'id'    => 'option_2',
-									'title' => 'Option 2'
-								),
-								array(
-									'id'    => 'option_3',
-									'title' => 'Option 3'
-								)
-							),
-							'default' => 'option_2'
-						),
-						array(
-							'id'          => 'radio',
-							'title'       => 'Radio',
-							'type'        => 'radio',
-							'description' => 'This is an example radio setting.',
-							'options'     => array(
-								array(
-									'id'    => 'option_1',
-									'title' => 'Option 1'
-								),
-								array(
-									'id'    => 'option_2',
-									'title' => 'Option 2'
-								),
-							),
-							'default' => 'option_2'
-						)
 					),
 					'context'  => 'normal',
 					'priority' => 'default'
@@ -122,7 +49,7 @@ if( ! class_exists( 'Plugin_Meta' ) ) {
 		 */
 		private function load_dependencies() {
 
-			require_once dirname( __FILE__ ) . '/libraries/class-alxwp-meta.php';
+			require_once dirname( __FILE__ ) . '/libraries/metabun/class-metabun.php';
 
 		}
 
